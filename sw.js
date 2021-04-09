9134d3c0085149a19e078edd789bd7a2faeedcf9
“Rob <github@dtsgroup.ca>
2021-04-09 14:59:07 -0600

    init

---
+++ sw.js
@@ -0,0 +1,16 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('PowerCALC').then((cache) => cache.addAll([
+      '/PowerCALC/',
+      '/PowerCALC/index.html',
+      '/PowerCALC/PowerCALC Logo.svg'
+    ])),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});
